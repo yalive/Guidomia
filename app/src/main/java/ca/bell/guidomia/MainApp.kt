3b81a6faff8bf31d18ebe0e6568cb6cf43a03de2
abdelhadi <abdelhadibahddou@gmail.com>
2022-08-24 15:51:14 +0100

    Sample data directory for UI preview.

--- app/src/main/java/ca/bell/guidomia/MainApp.kt
+++ app/src/main/java/ca/bell/guidomia/MainApp.kt
@@ -1,18 +1,16 @@
 package ca.bell.guidomia
 
 import android.app.Application
-import ca.bell.guidomia.di.AppComponent
 import ca.bell.guidomia.di.DaggerAppComponent
 
 class MainApp : Application() {
 
-    val appComponent: AppComponent by lazy { DaggerAppComponent.factory().create(this) }
+    val appComponent by lazy { DaggerAppComponent.factory().create(this) }
 
     override fun onCreate() {
         super.onCreate()
         INSTANC = this
     }
-
 
     companion object {
         private lateinit var INSTANC: MainApp
